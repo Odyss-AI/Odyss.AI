e539f63c02048be5f332324c9d6d77ac6cea207e
Erik Fitterling <e.fitterling@gmx.de>
2024-10-22 21:45:51 +0200

    refactor document manager

--- Odyss.AI.Backend/app/services/document_manager.py
+++ Odyss.AI.Backend/app/services/document_manager.py
@@ -50,25 +50,22 @@ class DocumentManager:
             converted_file = convert_docx_or_pptx_to_pdf(file)
 
             # Save PDF temporarly on shared volume for give OCR service access to the file
-            path = "shared_data/" + username + "_" + filename 
-            with open(path, "w") as file:
-                file.write(converted_file)
-            fileid = await db.upload_pdf(converted_file, file.filename, id)
-
+            path = self.get_temp_path(username, filename)
+            fileid = await self.save_file(converted_file, path, db, id)
             if fileid is None:
+                logging.error(f"Error creating embeddings: {file.filename} from user {username}")
                 return None, "Error while saving document in DB"
-            
-            # Create a new document object with necessary metadata
-            new_doc = self.get_new_doc(filename, fileid, file.filename, path)
 
             # Get all PDF informations (text/images)
-            new_doc = await extract_pdf_information_with_ocr(new_doc)
-
+            new_doc = await extract_pdf_information_with_ocr(self.get_new_doc(filename, fileid, file.filename, path))
+            if new_doc is None:
+                logging.error(f"Error extracting information: {file.filename} from user {username}")
+                return "Error extracting information while using ocr"
+            
             # TODO: Upload extracted prictures to mongoDB
 
             # Create embeddings for the document
             embeddings = await self.sim_search.create_embeddings_async(new_doc)
-
             if embeddings is None:
                 logging.error(f"Error creating embeddings: {file.filename} from user {username}")
                 return None, "Error creating embeddings"
@@ -81,22 +78,13 @@ class DocumentManager:
 
             # Create a summary for the document
             prompt = summary_prompt_builder(new_doc.textList)
-            summary = await call_mistral_api_async(prompt)
-            new_doc.summary = summary
-
+            new_doc.summary = await call_mistral_api_async(prompt)
             if new_doc.summary is None:
                 logging.error(f"Error creating summary: {file.filename} from user {username}")
                 return None, "Error creating summary"
 
-
-            # für die Texte werden Embeddings erstellt und in VektorDb gespeichert
-            #embeddings = await self.create_text_embedding_async(new_doc)
-
-            # Summary is generated from the text through LLM
-
             # Save new_doc in the database
             doc_id = await db.add_document_to_user_async(username, new_doc)
-
             if doc_id is None:
                 logging.error(f"Error saving document: {file.filename} from user {username}")
                 return None, "Error saving document"
@@ -118,48 +106,19 @@ class DocumentManager:
         """
 
         file_hash = hashlib.sha256(original_filename.encode('utf-8')).hexdigest()
-        # Save the file extension
         _, file_extension = os.path.splitext(original_filename)
         return f"{file_hash}{file_extension}", file_hash
-
-    async def save_document_local_async(self, file, filename):
-        """
-        Saves the document locally asynchronously.
-
-        Args:
-            file: The file object to be saved.
-            filename (str): The name of the file to be saved.
-
-        Returns:
-            str: The file path where the document is saved, or None if the file already exists.
-        """
-        
-        filepath = os.path.join(self.local_file_path, filename)
-        
-        if os.path.exists(filepath):
-            return None
-        
-        loop = asyncio.get_event_loop()
-        content = await loop.run_in_executor(None, file.read)  
-        
-        async with aiofiles.open(filepath, 'wb') as out_file:
-            await out_file.write(content) 
-        
-        return filepath
+    
+    def get_temp_path(self, username, filename):
+        return "shared_data/" + username + "_" + filename 
+    
+    async def save_file(self, converted_file, path, db, id):
+        with open(path, "w") as file:
+            file.write(converted_file)
+        fileid = await db.upload_pdf(converted_file, file.filename, id)
+        return fileid
     
     def get_new_doc(self, name: str, fileId: str, original_name: str, path: str):
-        """
-        Creates a new Document object with the provided metadata.
-
-        Args:
-            name (str): The unique name of the document.
-            path (str): The file path of the document.
-            original_name (str): The original name of the document.
-
-        Returns:
-            Document: The created Document object.
-        """
-        
         return Document(
             id = str(ObjectId()),
             doc_id=name,
@@ -171,26 +130,3 @@ class DocumentManager:
             textList = [],
             path = path
         )
-    
-    async def fetch_embedding_async(self, to_embed: str):
-        async with aiohttp.ClientSession() as session:
-            async with session.post(self.tei_url, json={"inputs": to_embed}) as response:
-                if response.status == 200:
-                    return await response.json().get("embedding")
-                return None
-    
-    async def create_text_embedding_async(self, doc: Document):
-        tasks = []
-        for chunk in doc.textList:
-            tasks.append(self.fetch_embedding_async(doc.id, chunk.text))
-        
-        embeddings = await asyncio.gather(*tasks)
-        
-        return embeddings
-    
-    async def save_embedding_async(self, doc_id: str, embedding: str):
-        # Speichere das Embedding in der VektorDb
-        pass
-
-        
-    
