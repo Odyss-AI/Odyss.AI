927b5d83612e65f30567b1711721e50c06a95583
oppelfe89127 <oppelfe89127@th-nuernberg.de>
2024-12-03 17:27:12 +0100

    added print to qdrant

--- Odyss.AI.Backend/app/services/sim_search_service.py
+++ Odyss.AI.Backend/app/services/sim_search_service.py
@@ -91,6 +91,7 @@ class SimailaritySearchService:
             bool: True if the embeddings were successfully saved, False otherwise.
         """
         try:
+            print("saving process started qdrant")
             points = []
             for embedding in embeddings:
                 points.append(PointStruct(id=str(uuid.uuid4()), vector=embedding[0], payload={"doc_id": id, "chunk_id": embedding[1]}))
