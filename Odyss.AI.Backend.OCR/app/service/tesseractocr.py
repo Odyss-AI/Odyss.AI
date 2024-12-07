3d9f588c3d05f2bbe50440b05f2e1d1b75cfaad4
ramazanovem88811 <ramazanovem88811@th-nuernberg.de>
2024-12-07 21:26:38 +0100

    poppler_path set

--- Odyss.AI.Backend.OCR/app/service/tesseractocr.py
+++ Odyss.AI.Backend.OCR/app/service/tesseractocr.py
@@ -38,7 +38,7 @@ class OCRTesseract:
             print("Extrahiere Text aus PDF mit Tesseract OCR...")
             
             # Konvertiere PDF-Seiten in Bilder
-            images = convert_from_bytes(pdf_stream.getvalue(), fmt='JPEG')
+            images = convert_from_bytes(pdf_stream.getvalue(), fmt='JPEG', poppler_path="/usr/bin")
 
             for page_num, image in enumerate(images):
                 print(f"Verarbeite Seite {page_num + 1} mit Tesseract OCR...")
@@ -60,7 +60,6 @@ class OCRTesseract:
                     print(f"Kein Text auf Seite {page_num + 1} erkannt.")
         except Exception as e:
             print(f"Fehler beim Extrahieren des Textes aus dem PDF: {e}")
-
 
     def extract_images_from_pdf(self, pdf_stream, doc):
         try:
