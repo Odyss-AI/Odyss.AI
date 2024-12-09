bfa52f4410b110f00b911877df96973b534d6053
oppelfe89127 <oppelfe89127@th-nuernberg.de>
2024-12-09 10:53:25 +0100

    debug sim_search

--- Odyss.AI.Backend/app/services/sim_search_service.py
+++ Odyss.AI.Backend/app/services/sim_search_service.py
@@ -70,9 +70,9 @@ class SimailaritySearchService:
         tasks = []
         for chunk in doc.textList:
             tasks.append(self.fetch_embedding_async(chunk.text, chunk.id))
-        for img in doc.imgList:
-            if(img.imgtext):
-                tasks.append(self.fetch_embedding_async(img.imgtext, img.id))
+        #for img in doc.imgList:
+            #if(img.imgtext):
+                #tasks.append(self.fetch_embedding_async(img.imgtext, img.id))
             #if(img.llm_output):
             #    tasks.append(self.fetch_embedding_async(img.llm_output, img.id))
         
