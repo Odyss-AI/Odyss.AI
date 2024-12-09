f8b2fee07732a759604ece850df5a2f1837ca81a
oppelfe89127 <oppelfe89127@th-nuernberg.de>
2024-12-09 17:49:22 +0100

    added time needed to document manager

--- Odyss.AI.Backend/app/services/document_manager.py
+++ Odyss.AI.Backend/app/services/document_manager.py
@@ -5,6 +5,7 @@ import aiohttp
 import asyncio
 import uuid
 import logging
+import time
 
 from bson import ObjectId
 
@@ -44,6 +45,8 @@ class DocumentManager:
         
         try:
             db = get_db()
+            start = time.time()
+            parttime = time.time()
             # Generate a unique name for the document
             hash_doc, hash = self.generate_filename(file.filename)
             try:
@@ -51,12 +54,15 @@ class DocumentManager:
                 converted_file_path, converted_file = await save_and_convert_file(file, hash_doc, db)
             except Exception as e:
                 logging.error("Error while converting file: "+e)
-
+            print("Dauer zum konvertieren und Speichern der Datei: ", time.time()-parttime)
+            parttime = time.time()
             try:
                 # Upload the PDF to MongoDB and get the file objectID back
                 mongo_file_id = await db.upload_pdf_async(converted_file, file.filename, hash, username)
             except Exception as e:
                 logging.error("Error while uploading file to Mongodb: "+ e)
+            print("Dauer zum hochladen der Datei in die Datenbank: ", time.time()-parttime)
+            parttime = time.time()
             print("started OCR")
             try:
                 # Get all PDF informations (text/images)
@@ -64,7 +70,9 @@ class DocumentManager:
                 new_doc = await extract_pdf_information_with_ocr(new_doc)
             except Exception as e:
                 logging.error("Error while extracing information while using ocr: "+e)
-
+            print("OCR finished")
+            print("Dauer zum extrahieren der Informationen: ", time.time()-parttime)
+            parttime = time.time()
             # TODO: Upload extracted pictures to mongoDB (maybe not necessary anymore)
             print("started imaged information extraction")
             try:
@@ -77,6 +85,8 @@ class DocumentManager:
             except Exception as e:
                 logging.error("Error while Image evaluation and deleting: "+e)
             print("images tagged and deleted")
+            print("Dauer zum taggen und löschen der Bilder: ", time.time()-parttime)
+            parttime = time.time()
             print("started embedding creation")
             try:
                 # Create embeddings for the document
@@ -86,6 +96,8 @@ class DocumentManager:
             except Exception as e:
                 logging.error("Error while creating embeddings: "+e)
             print("embeddings created")
+            print("Dauer zum erstellen der Embeddings: ", time.time()-parttime)
+            parttime = time.time()
             print("started saving embeddings")
             try:
                 # Save the embeddings in QDrant
@@ -95,6 +107,8 @@ class DocumentManager:
             except Exception as e:
                 logging.error("Error while saving embeddings in QDrant: "+e)
             print("embeddings saved")
+            print("Dauer zum speichern der Embeddings: ", time.time()-parttime)
+            parttime = time.time()
             print("started summary creation")
             try:
                 # Create a summary for the document
@@ -103,6 +117,8 @@ class DocumentManager:
             except Exception as e:
                 logging.error("Error while creating summary: "+e)
             print("summary created")
+            print("Dauer zum erstellen der Zusammenfassung: ", time.time()-parttime)
+            parttime = time.time()
             print("started document saving")
             try:
                 # Save new_doc in the database
@@ -110,6 +126,8 @@ class DocumentManager:
             except Exception as e:
                 logging.error("Error while saving document: "+e)
             print("document saved")
+            print("Dauer zum speichern des Dokuments: ", time.time()-parttime)
+            parttime = time.time()
             print("started adding document to chat")
             try:
                 is_doc_added_to_chat = await db.add_document_to_chat_async(chat_id, hash)
@@ -118,7 +136,8 @@ class DocumentManager:
             except Exception as e:
                 logging.error("Error while adding document to chat: "+e)
             print("document added to chat")
-
+            print("Dauer zum hinzufügen des Dokuments zum Chat: ", time.time()-parttime)
+            print("Gesamtdauer: ", time.time()-start)
             return new_doc, "File uploaded successfully"
         except Exception as e:
             logging.error(f"Error handling document {file.filename} from {username}: {str(e)}")
