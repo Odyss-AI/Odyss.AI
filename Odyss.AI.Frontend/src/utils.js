0a311f5b36fdf0c13bfe4f59fa6c643d01907d7f
odyss <odyss@th-nuernberg.de>
2025-02-12 13:08:24 +0100

    setup local env

--- Odyss.AI.Frontend/src/utils.js
+++ Odyss.AI.Frontend/src/utils.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BaseUrl = "http://141.75.150.145:443";
+// const BaseUrl = "http://141.75.150.145:443";
+const BaseUrl = "http://0.0.0.0:8443";
 
 // GET-Anfrage
 const getUser = async (user) => {
@@ -19,6 +20,7 @@ const getUser = async (user) => {
 }
 
 const createUser = async (user) => {
+    console.log(user);
     try{
         const response = await axios.post(`${BaseUrl}/v1/user/add`, {
             username: user
